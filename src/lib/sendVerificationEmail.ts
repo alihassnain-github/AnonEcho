de78015714d1e08c3b96fa7d0c1817e40b774071
alihassnain-github <alimurtaza32721@gmail.com>
2025-03-24 21:57:43 +0500

    send verification email utility created.

---
+++ src/lib/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import VerifyEmail from "@/components/email-template";
+import { resend } from "./resend";
+
+interface sendVerificationEmailProps {
+    username: string,
+    email: string,
+    otp: string,
+}
+
+export default async function sendVerificationEmail({ username, email, otp }: sendVerificationEmailProps) {
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Verify Your Email Address',
+            react: VerifyEmail({ username, otp }),
+        });
+        return { success: true, message: "Verification email send successfuly" };
+    } catch (error) {
+        return { success: false, message: "Failed to send Verification email" };
+    }
+}
